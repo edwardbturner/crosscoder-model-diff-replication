2a47a84f78d99f4ce01d20fbac6bca05f1f5fbda
Edward Turner <edward.turner01@outlook.com>
2025-03-06 13:54:31 +0100

    amends

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ def get_model(name: str, device: str) -> HookedTransformer:
     return HookedTransformer.from_pretrained(name, device=device)
 
 
-def get_default_cfg(base_model: HookedTransformer) -> dict:
+def get_default_cfg(base_model: HookedTransformer, device: str, wandb_project: str, wandb_entity: str) -> dict:
     default_cfg = {
         "seed": 49,
         "batch_size": 4096,
@@ -22,24 +22,23 @@ def get_default_cfg(base_model: HookedTransformer) -> dict:
         "dict_size": 2**14,
         "seq_len": 1024,
         "enc_dtype": "fp32",
-        "model_name": "gemma-2-2b",
+        "model_name": base_model.cfg.model_name,
         "site": "resid_pre",
-        "device": "cuda:0",
+        "device": device,
         "model_batch_size": 4,
         "log_every": 100,
         "save_every": 30000,
         "dec_init_norm": 0.08,
         "hook_point": "blocks.14.hook_resid_pre",
-        "wandb_project": "YOUR_WANDB_PROJECT",
-        "wandb_entity": "YOUR_WANDB_ENTITY",
+        "wandb_project": wandb_project,
+        "wandb_entity": wandb_entity,
     }
     return arg_parse_update_cfg(default_cfg)
 
 
-def get_default_trainer() -> Trainer:
-    device = "cuda:0"
+def get_default_trainer(device: str, wandb_project: str, wandb_entity: str) -> Trainer:
     base_model = get_model("gemma-2-2b", device)
     chat_model = get_model("gemma-2-2b-it", device)
-    cfg = get_default_cfg(base_model)
+    cfg = get_default_cfg(base_model, device, wandb_project, wandb_entity)
     all_tokens = load_pile_lmsys_mixed_tokens()
     return Trainer(cfg, base_model, chat_model, all_tokens)
